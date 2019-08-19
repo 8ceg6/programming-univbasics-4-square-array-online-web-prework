688ccd3d9fa65e2b61ac1f520822ef5ca2a03214
cedric e goddard <8ceg6@comcast.net>
2019-08-19 12:14:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,6 @@
 
 def square_array(numbers)
+<<<<<<< HEAD
     new_array=[]
     counter=0 
     
@@ -11,6 +12,9 @@ def square_array(numbers)
 	 
 	new_array 
 	 
+=======
+  numbers=
+>>>>>>> e60c1a69c4aa31dc8335e46a8af3e0f7f7746c5a
 	 
   end
   
